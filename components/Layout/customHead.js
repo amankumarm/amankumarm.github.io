8bab41efae370900671cf39ac47670441211fa12
amankumarm <amankumarm441@gmail.com>
2022-07-08 12:13:14 +0530

    seo changes

---
+++ components/Layout/customHead.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function customHead() {
+  return (
+    <Head>
+      <title>PortFolio | Aman</title>
+      <meta name="keywords" content="Aman Kumar M, Aman , Kumar Aman" />.
+    </Head>
+  )
+}
--- components/Layout/index.js
+++ components/Layout/index.js
@@ -3,9 +3,11 @@ import Head from 'next/head'
 import Navbar from './navbar'
 import LeftSideBar from './left_sidebar'
 import RightSideBar from './right_sidebar'
+import CustomHead from './customHead'
 function Index(props) {
   return (
     <>
+      <CustomHead />
       <div className=" h-screen w-full ph:h-[90vh] ">
         <Navbar />
         <div className="absolute grid h-[90%] w-full grid-cols-12">
