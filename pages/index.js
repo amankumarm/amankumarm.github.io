aa3ea87f77244303996086dd5c3ca8a3e8d882c2
amankumarm <amankumarm441@gmail.com>
2022-07-08 12:28:50 +0530

    added resume

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 import tableguy from '../public/assets/images/table.png'
 function Index() {
   return (
-    <div className=" px-20 ph:items-start flex h-full flex-row items-center bg-bgBlue  py-12 ph:p-8 font-roboto text-white ph:flex-col-reverse ph:justify-between ">
-      <div className="w-1/2 ph:w-full px-6 ph:px-4">
+    <div className=" flex h-full flex-row items-center bg-bgBlue px-20 py-12  font-roboto text-white ph:flex-col-reverse ph:items-start ph:justify-between ph:p-8 ">
+      <div className="w-1/2 px-6 ph:w-full ph:px-4">
         <p>Hi there,</p>
-        <p className="py-4 text-7xl font-bold font-roboto text-blueGreen">I'm Aman</p>
+        <p className="py-4 font-roboto text-7xl font-bold text-blueGreen">
+          I'm Aman
+        </p>
         <p className="text-sm leading-relaxed tracking-wider">
           I'm Aman Kumar M , 20 years old and from India. building Web-apps has
           been an interest of mine for many years. All the skills and tools I
@@ -14,9 +16,14 @@ function Index() {
         </p>
         <div className="flex flex-row gap-5 pt-8">
           <div className="delay-125 cursor-pointer border-2 border-blueGreen p-2 px-3 transition ease-in-out hover:scale-110 hover:bg-blueGreen hover:text-bgBlue">
-            Resume
+            <a href="/resume.pdf" download={true}>
+              Resume
+            </a>
           </div>
-          <div className="delay-125 cursor-pointer border-2 border-blueGreen p-2 px-3 transition ease-in-out hover:scale-110 hover:bg-blueGreen hover:text-bgBlue">
+          <div
+            className="delay-125 cursor-pointer border-2 border-blueGreen p-2 px-3 transition ease-in-out hover:scale-110 hover:bg-blueGreen hover:text-bgBlue"
+            onClick={() => (window.location.href = '/contact')}
+          >
             Hire Me
           </div>
         </div>
