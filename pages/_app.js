0f17a84506917fe35eeac8a9f756d8335fe06b90
amankumarm <amankumarm441@gmail.com>
2025-12-10 01:59:49 +0530

    sc

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout/index'
+import { Analytics } from '@vercel/analytics/next'
+
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    // <Layout>
+    //   <Component {...pageProps} />
+    // </Layout>
+    <div className="flex h-[100svh] items-center justify-center">
+      <p>Will be right back!!</p>
+    </div>
   )
 }
 
