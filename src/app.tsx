32370dbe6535706c28c585fcc9ce0aa5599a74e0
amankumarm <amankumarm441@gmail.com>
2025-12-10 01:55:57 +0530

    sc

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-svh flex items-center justify-center">
+      <p>Will be right back!!</p>
+      
+    </div>
+  );
+}
+
+export default App;
