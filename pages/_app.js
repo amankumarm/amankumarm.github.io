39b12c731f2ab6abd79ed1f7325c56acce1881ae
amankumarm <amankumarm441@gmail.com>
2025-12-10 02:00:48 +0530

    sc

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
 import '../styles/globals.css'
-import Layout from '../components/Layout/index'
-import { Analytics } from '@vercel/analytics/next'
 
 function MyApp({ Component, pageProps }) {
   return (
